38b1dd651aa7bb410408fbed992940b1ed470233
Hackbright Student <info@hackbrightacademy.com>
2014-10-16 11:39:45 -0700

    first commit

---
+++ exercise01.js
@@ -0,0 +1,13 @@
+var sum = 0;
+
+for (i=1; i<1000; i++){
+    if (i%3 === 0) {
+        sum = sum + i;
+        }
+    else if (i%5 === 0)
+        {
+        sum = sum + i;
+        }
+    }
+
+console.log(sum); 
